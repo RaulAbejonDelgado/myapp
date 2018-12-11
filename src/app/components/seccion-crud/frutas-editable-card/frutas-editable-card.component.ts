1c1cc49eee9fcbedb45cecc4bb8f5d1a8ab63fee
rabejon <rabejon@kernet.es>
2018-12-11 08:03:53 +0100

    colores

--- src/app/components/seccion-crud/frutas-editable-card/frutas-editable-card.component.ts
+++ src/app/components/seccion-crud/frutas-editable-card/frutas-editable-card.component.ts
@@ -29,24 +29,33 @@ export class FrutasEditableCardComponent implements OnInit {
     } 
   }
 
+  colorObjeto:Object = {
+    "color":""
+  };
   id: number;
   formulario : FormGroup;
   color : FormControl;
   colorNuevo : FormControl;
+  frutaEditable : Frutas;
   
 
   constructor(private route: ActivatedRoute,public frutaService : FrutaService) {
+    this.frutaEditable = new Frutas();
     this.color= new FormControl();
     this.colorNuevo = new FormControl();
     this.formulario = new FormGroup({
+      id : new FormControl(),
       nombre : new FormControl('',[Validators.required, Validators.minLength(2), Validators.maxLength(45)]),
       precio : new FormControl(0,[Validators.required,Validators.minLength(1),Validators.maxLength(5)]),
       calorias : new FormControl(0,[Validators.required,Validators.minLength(0)]),
-      colores: new FormArray(  [this.crearColorFormGroup()] ),// quito para que inicialmente solo aparezca un campo color al inicializarse, this.crearColorFormGroup(),
+      colores: new FormArray([this.crearColorFormGroup()]),// quito para que inicialmente solo aparezca un campo color al inicializarse, this.crearColorFormGroup(),
       oferta : new FormControl(false),
       descuento : new FormControl(0),
       imagen: new FormControl('https://picsum.photos/300/300/?random', [ Validators.required, Validators.pattern('^(http(s?):\/\/).+(\(.png|.jpg|j.peg|random))$')]),
-      coloresNuevos : new FormArray([ this.crearColorNuevoFormGroup() ])})
+      coloresNuevos : new FormArray([ ])
+    })
+      
+      
    }
 
   ngOnInit() {
@@ -56,11 +65,26 @@ export class FrutasEditableCardComponent implements OnInit {
       // llamar provider para conseguir datos a traves del id
       this.obtenerPorId(this.id);
     });
+    
+  }
+  
+
+  cargarFormualario(){
+    
+    console.log(this._objeto);
+    console.log(this.formulario);
+    this.formulario.setValue(this._objeto);
+    // console.log(this.formulario.value);
+    this.formulario.controls.nombre.setValue(this._objeto.nombre);
+    this.formulario.controls.precio.setValue(this._objeto.precio);
+    
+    
   }
 
   obtenerPorId(id:number){
     this.frutaService.obtenerFrutaPorId(id).subscribe(data =>{
       this._objeto = data;
+      this.cargarFormualario();
     });
 
   }
@@ -84,29 +108,30 @@ export class FrutasEditableCardComponent implements OnInit {
     console.log(this.color.value);
     return new FormGroup({
                 
-                color: new FormControl(this.color.value)
+                color: new FormControl(this.colorNuevo.value)
         });
   }
 
-  crearColorNuevoFormGroup(){
-    console.log(this.colorNuevo.value);
-    return new FormGroup({
-        colorNuevo : new FormControl(this.colorNuevo.value)
-    })
-  }
+  // crearColorNuevoFormGroup(){
+  //   console.log(this.colorNuevo.value);
+  //   return new FormGroup({
+  //       colorNuevo : new FormControl(this.colorNuevo.value)
+  //   })
+  // }
 
   nuevoColor(){    
     let arrayColores = this.formulario.get('coloresNuevos') as FormArray;
     let arrayObjetoColores = this._objeto.colores;
     console.log(arrayColores);
     console.log(this._objeto.colores);
+    console.log(this.colorNuevo);
 
-    arrayColores.push(new FormGroup({color: new FormControl(this.color.value)}));
+    arrayColores.push(new FormGroup({color: new FormControl(this.colorNuevo.value)}));
   }
 
   eliminarColorExistente(index:number){
     let arrayColores = this.formulario.get('coloresNuevos') as FormArray;
-    if ( this.objeto.colores.length > 1 ){
+    if ( this.objeto.colores.length > 0 ){
       this.objeto.colores.splice(index, 1);
       console.log(this.objeto.colores);
     }
@@ -115,38 +140,48 @@ export class FrutasEditableCardComponent implements OnInit {
 
   eliminarColorNuevo( index: number){
     let arrayColores = this.formulario.get('coloresNuevos') as FormArray;
-    if ( arrayColores.length > 1 ){
+    if ( arrayColores.length > 0 ){
       arrayColores.removeAt(index);
     }  
   }
 
   actualizar(){
     console.log("modificar - sumitar %o",  this.formulario);
-    let fruta = new Frutas();
+    //let fruta = new Frutas();
 
-    fruta.id = this.id;
-    fruta.nombre = this.formulario.controls.nombre.value;
-    fruta.precio = this.formulario.controls.precio.value;
-    fruta.calorias = this.formulario.controls.calorias.value;
-    fruta.colores = this.formulario.controls.colores.value;
-    //agregamos los valores
-    this.formulario.controls.coloresNuevos.value.forEach(element => {
-      
-        fruta.colores.push(element);
-          
-    });
-    fruta.descuento = this.formulario.controls.descuento.value;
-    fruta.imagen = this.formulario.controls.imagen.value;
-    fruta.oferta = this.formulario.controls.oferta.value;
-    this.frutaService.actualizar(fruta).subscribe(data =>{
+    this.frutaEditable.id = this.id;
+    this.frutaEditable.nombre = this.formulario.controls.nombre.value;
+    this.frutaEditable.precio = this.formulario.controls.precio.value;
+    this.frutaEditable.calorias = this.formulario.controls.calorias.value;
+    // fruta.colores = this.formulario.controls.coloresNuevos.value;
+    this.juntarColores();
+    this.frutaEditable.descuento = this.formulario.controls.descuento.value;
+    this.frutaEditable.imagen = this.formulario.controls.imagen.value;
+    this.frutaEditable.oferta = this.formulario.controls.oferta.value;
+    this.frutaService.actualizar(this.frutaEditable).subscribe(data =>{
       console.debug(data);
       this._objeto = data;
     })
-
-  
+    let arrayColores = this.formulario.get('coloresNuevos') as FormArray;
+    arrayColores.controls.splice(0);
     
-
   }
+
+  juntarColores(){
+      let arrayColores = this.formulario.get('coloresNuevos') as FormArray;
+      if (arrayColores.length == 0){
+        this.frutaEditable.colores = this.objeto.colores;
+      }
+      if(arrayColores.length > 0){
+
+        for(let color of arrayColores.controls){
+       
+          this.frutaEditable.colores = this._objeto.colores;
+          this.frutaEditable.colores.push(color.value);
+        }
+      }
+    }
+  
   agregarColores(value: any): any {
     value.array.forEach(element => {
       
@@ -174,7 +209,9 @@ export class FrutasEditableCardComponent implements OnInit {
   
   editarColor(indice:number, color:string){
     console.log(this.color)
-    this.objeto.colores[indice] = this.color.value;
+    let arrayColores = this.formulario.get('coloresNuevos') as FormArray;
+    //this.objeto.colores[indice] = color;
+    arrayColores.push(new FormGroup({color: new FormControl(color)}));
 
     
   }
